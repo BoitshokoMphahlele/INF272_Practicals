dcb64bf535ce8acf72fa2bb29837597dae8633ed
Boitshoko Mphahlele <boitshokontshoekgolo@gmail.com>
2025-03-09 17:21:56 +0200

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="u24634400_INF272_Prac2.MvcApplication" Language="C#" %>
---
+++ Models/MyModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace u24634400_INF272_Prac2.Models
+{
+    public class MyModel
+    {
+        [Display(Name = "Student Number")]
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Link to personal page")]
+        public string mylink { get; set; }
+    }
+}
